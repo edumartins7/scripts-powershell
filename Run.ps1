fb52ed080a99ec3b918cf9fc195cd4e35ebf8cd4
Eduardo Moreira <edumartins1w@gmail.com>
2016-07-30 23:03:19 -0300

    .

---
+++ Run.ps1
@@ -0,0 +1,21 @@
+﻿$CurrentDir = $(get-location).Path
+$templateFolderPath = $CurrentDir + "\modelo\"
+$outputFolderPath = $CurrentDir + "\out\"
+
+$lowCaseKey = "~template~"
+$upperCaseKey = "~Template~"
+$pluralKey = "^plural"
+
+$newLowCaseValue = "foo"
+$newUpperCaseValue = "Foo"
+$newPluralValue = @("foos", "Foos")
+
+
+Write-Host $templateFolderPath
+
+Remove-Item $outputFolderPath + "*" -Force -Recurse
+
+Copy-Item $templateFolderPath $outputFolderPath -recurse
+
+Write-Host "Copiado com sucesso! Pressione qualquer tecla para sair."
+Read-Host
